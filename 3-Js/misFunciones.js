0d1de923f8e1ddf87ef00e6b3c44265ab54bcf0d
Bautista Chacon <bautistachacon05@gmail.com>
2026-09-19 21:53:42 -0300

    hice los ejercicios de hola mundo y conversor de unidades

--- 3-Js/misFunciones.js
+++ 3-Js/misFunciones.js
@@ -5,3 +5,19 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+
+function cambiarunidades(id,valor){
+if (isNaN(valor)) {
+    alert("se ingreso un valor invalido");
+    document.lasunidades.unid_metro.value="";
+    document.lasunidades.unid_pulgada.value="";
+    document.lasunidades.unid_pie.value="";
+    document.lasunidades.unid_yarda.value="";
+
+} else if (id=="metro") {
+    document.lasunidades.unid_pulgada.value=39.3701*valor;
+    document.lasunidades.unid_pie.value=3.28084*valor;
+    document.lasunidades.unid_yarda.value=1.09361*valor;
+}
+
+}
